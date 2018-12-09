d6584242dffd606c859799be6842ddd3bbf9fd0b
bonnina <37539513+bonnina@users.noreply.github.com>
2018-12-09 15:33:55 +0200

    Add routes and settings (move from REST-API-MoviesApp)

--- server/app.js
+++ server/app.js
@@ -3,6 +3,12 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const fileUpload = require('express-fileupload');
+const helmet = require('helmet');
+const cors = require('cors');
+const moviesRouter = require('./routes/movies');
+const starsRouter = require('./routes/stars');
+const uploadRouter = require('./routes/upload');
 
 const app = express();
 
@@ -14,7 +20,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+app.options(process.env.WHITELIST, cors());  
+app.use(helmet());
+app.use(fileUpload());  
 
+app.use('/movies', moviesRouter);
+app.use('/stars', starsRouter);
+app.use('/upload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -23,11 +36,9 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
